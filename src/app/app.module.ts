5a7c6c136d7fc5678d50383d73b64bb103c6996f
David Sanchez <d.sanchez-rodriguez@craneware.com>
2018-08-17 17:20:37 +0100

    Templates, Interpolation, Directives Module Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   
   ],
   bootstrap: [AppComponent]
 })
