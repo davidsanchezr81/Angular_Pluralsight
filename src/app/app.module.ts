fe1b2c98fbf9c9e089870ca30d2a967e8e7c2994
David <davidsanchezr81@gmail.com>
2018-08-18 16:31:07 +0100

    Starting Data Binding & Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from '../api/products/product-list.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductListComponent
     
   ],
   imports: [
