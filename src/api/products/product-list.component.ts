fbabd651eb12b8f7e3701785189eac316c436c61
David <davidsanchezr81@gmail.com>
2018-08-22 20:56:05 +0100

    More on Components completes

--- src/api/products/product-list.component.ts
+++ src/api/products/product-list.component.ts
@@ -12,7 +12,17 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage:boolean = false;
-    listFilter: string = 'cart';
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value:string){
+        this._listFilter = value;
+        this.filteredProducts= this.listFilter ? this.performFilter(this.listFilter): this.products;
+    }
+    
+    filteredProducts: IProduct[];
     products: IProduct[] =[
         {
             "productId": 1,
@@ -35,6 +45,17 @@ export class ProductListComponent implements OnInit {
             "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
           }
     ];
+
+    constructor(){  // these are the default values
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+    
+    performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();  // it converts the filter criteria to lower case. We want a case insensitive comparison
+        return this.products.filter((product: IProduct) => // this return the filtered list of products
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
     
     toggleImage(): void {
         this.showImage = !this.showImage;
