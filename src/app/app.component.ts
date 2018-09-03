c3b57f7f369026950c4d515ed8a94fca7d194ff8
David Sanchez <d.sanchez-rodriguez@craneware.com>
2018-09-03 16:43:29 +0100

    Navigation and Routing Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,14 @@ import { Component } from '@angular/core';
   <ul class='nav nav-pills'>
     <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
     <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
-  </ul>`,
+  </ul>
+  </nav>
+  <div class='container'>
+  <router-outlet></router-outlet> 
+  </div>
+  `
+
+  // whenever a route is activated, the associated view will display the content in this div (router-outlet)
 
 })
 export class AppComponent {
